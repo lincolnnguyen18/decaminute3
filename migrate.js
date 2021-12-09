cc3262608cd74b5b4359a885457b0f80928dfb45
lincolnnguyen18 <letsmakeaworld@icloud.com>
2021-12-09 11:35:31 -0600

    update

--- migrate.js
+++ migrate.js
@@ -3,14 +3,20 @@ var db = new sqlite3.Database('data.db');
 var db2 = new sqlite3.Database('data2.db');
 
 db.serialize(function() {
-  db2.run("drop table if exists users");
-  db2.run("drop table if exists decaminutes");
-  db2.run("create table if not exists decaminutes (time integer primary key not null, value integer not null, description text, userId integer not null, foreign key(userId) references users(id))");
-  db2.run("create table if not exists users (id integer primary key autoincrement not null, worked integer, postWorkedEnabled integer, total integer, timezoneOffset integer, username text, password text)");
-  db.all('select * from decaminutes', function(err, rows) {
+  // db2.run("drop table if exists users");
+  // db2.run("drop table if exists decaminutes");
+  // db2.run("create table if not exists decaminutes (time integer primary key not null, value integer not null, description text, userId integer not null, foreign key(userId) references users(id))");
+  // db2.run("create table if not exists users (id integer primary key autoincrement not null, worked integer, postWorkedEnabled integer, total integer, timezoneOffset integer, username text, password text)");
+  // db.all('select * from decaminutes', function(err, rows) {
+  //   rows.forEach(function(row) {
+  //     // console.log(row);
+  //     db2.run("insert into decaminutes (time, value, userId, description) values (?, ?, ?, ?)", row.time, row.value, 1, null);
+  //   });
+  // });
+  db.all('select * from users', function(err, rows) {
     rows.forEach(function(row) {
-      console.log(row);
-      db2.run("insert into decaminutes (time, value, userId, description) values (?, ?, ?, ?)", row.time, row.value, 1, 'N/A');
+      // console.log(row);
+      db2.run("insert into users (id, worked, postWorkedEnabled, total, timezoneOffset, username, password) values (?, ?, ?, ?, ?, ?, ?)", row.id, row.worked, row.postWorkedEnabled, row.total, row.timezoneOffset, 'lincolnnguyen', 'Ln2121809');
     });
   });
 }, function() {
