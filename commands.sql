a7b9e903490fdeadfbf7ab14f3a48ab5bee3a5e9
lincolnnguyen18 <letsmakeaworld@icloud.com>
2021-12-09 01:10:31 -0600

    update

--- commands.sql
+++ commands.sql
@@ -1,4 +1,5 @@
 create table if not exists decaminutes (time integer primary key not null, value integer not null)
+create table if not exists users (id integer primary key autoincrement not null, worked integer, postWorkedEnabled integer, total integer, timezoneOffset integer)
 
 insert into decaminutes (time, value) VALUES ((select max(time) from decaminutes)+600, (select max(value) from decaminutes)+2);
 delete from decaminutes order by time desc limit 1;
@@ -9,3 +10,5 @@ insert into decaminutes (time, value) VALUES ((select max(time) from decaminutes
 insert into decaminutes (time, value) VALUES ((select max(time) from decaminutes)+600, (select value from decaminutes order by time desc limit 1) - 1);
 
 select * from decaminutes;
+
+update users set total = (select value from decaminutes order by time desc limit 1);
